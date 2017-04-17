287fb19538656b04a6a84247ca344a444d0487db
Mohamadou <mohamadou@Mohamadous-MacBook-Pro.local>
2017-04-17 18:45:13 -0400

    Added mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,8 @@ class ContactMailer < ActionMailer::Base
         @name = name
         @email = email
         @body = body
+
+        mail(from: email, subject: 'Contact Form Message')
     end
 
 end
